b2cdba64f730ca0722c249190262e7aadfd16f25
gabatk <gabriela.trybuszek@gmail.com>
2022-12-29 22:33:44 +0100

    JavaScript functions implemented.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('search');
+const ingredients = document.querySelectorAll('.list-details');
+
+const search = () => {
+	let insertedValue = input.value.toLowerCase();
+	ingredients.forEach(element => {
+		if (!element.textContent.toLowerCase().includes(insertedValue)) {
+			element.parentElement.classList.add('hidden');
+		} else {
+			element.parentElement.classList.remove('hidden');
+		}
+	});
+};
+
+input.addEventListener('keyup', search);
